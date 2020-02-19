050a7686051efa5bbbe68f97a36eb4f00ffabcf0
vsuaste <suaste@cimat.mx>
2020-02-18 19:33:29 -0600

    helper async foreach

--- utils/helper.js
+++ utils/helper.js
@@ -312,3 +312,10 @@ f   *
       })
 
    }
+
+
+   module.exports.asyncForEach = async function(array, callback) {
+     for (let index = 0; index < array.length; index++) {
+       await callback(array[index], index, array);
+     }
+  }
