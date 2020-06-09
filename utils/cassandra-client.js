748fbdbc2acb6ca7f1f5e5d1e566291c5e6f9416
Thomas Voecking <thomas.voecking@auticon.de>
2020-06-09 18:49:02 +0200

    Separate host and port in the cassandra server string

--- utils/cassandra-client.js
+++ utils/cassandra-client.js
@@ -2,9 +2,12 @@ const driver = require('cassandra-driver');
 const globals = require('../config/globals');
 
 const client = new driver.Client({
-    contactPoints: [globals.CASSANDRA_HOST],
+    contactPoints: [globals.CASSANDRA_HOST + ':' + globals.CASSANDRA_PORT],
     localDataCenter: 'datacenter1',
-    keyspace: 'sciencedb'
+    keyspace: 'sciencedb',
+    protocolOptions: {
+        port: globals.CASSANDRA_PORT
+    }
 });
 
 module.exports = client;
