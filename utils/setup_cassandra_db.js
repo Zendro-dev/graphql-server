9fe48c7f81a9e655205b41ae25dd66dedc0c9e10
Thomas Voecking <thomas.voecking@auticon.de>
2020-06-12 13:09:15 +0200

    Process the Cassandra migration handler with Promise.all

--- utils/setup_cassandra_db.js
+++ utils/setup_cassandra_db.js
@@ -23,9 +23,10 @@ async function createTableMigrated() {
         }
     }
     if (migrateToDo) {
-      for await (let cassandraHandler of migrations_cassandra) {
+      /*for await (let cassandraHandler of migrations_cassandra) {
         cassandraHandler.up();
-      }
+      }*/
+      Promise.all(migrations_cassandra.map(async cassandraHandler => await cassandraHandler.up()));
       const createTable = "CREATE TABLE IF NOT EXISTS db_migrated ( migrated_at timeuuid PRIMARY KEY )";
       await client.execute(createTable);
       console.log('Migration table created');
