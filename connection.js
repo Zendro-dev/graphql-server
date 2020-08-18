6bfa7237961f9a3e3aec3f85c17cb1c22c0cfbb8
David Velasco <davvelsan@gmail.com>
2020-08-18 16:52:37 +0200

    feat: simplify connection config

--- connection.js
+++ connection.js
@@ -1,11 +1,9 @@
 const { Sequelize } = require('sequelize');
 const config = require('./config/data_models_storage_config.json');
 
-const env = process.env.NODE_ENV || 'development';
-const envConfig = config[env];
 
 const Op = Sequelize.Op;
-envConfig.operatorsAliases = {
+config.operatorsAliases = {
   $eq: Op.eq,
   $and: Op.and,
   $or: Op.or,
@@ -25,25 +23,12 @@ envConfig.operatorsAliases = {
 };
 
 /**
- * Stored sequelize instances. Consider using a WeakMap instead
- * so that Sequelize instances are garbage collected when there are no more
- * references to their key object. Not that it would require object keys.
+ * Stored sequelize instances.
  */
 const currentInstances = new Map()
 
 /**
  * Get a new or existing sequelize instance using the specified database.
- * Three possibilities exist depending on whether the "database" argument
- * is defined and has a matching value.
- *
- * - __UNDEFINED__: neither "database" nor "storageType" properties
- * are set in the data model config. An error is thrown.
- *
- * - __NO-MATCH__: the database does not match any known instance. One is
- * created and returned.
- *
- * - __MATCH__: the "database" matches a known instance, which is returned.
- *
  * @param {string|undefined} database database config for the sequelize instance
  * @returns A configured `new Sequelize` instance
  */
@@ -55,13 +40,12 @@ module.exports.getConnection = (database) => {
     'Verify that "config/data_models_storage_config.json" is correctly set.'
   )
 
-  // Perform a case-insensitive match (this might not be ideal)
+  // Perform a case-insensitive match
   const _database = database.toLowerCase();
-  if (!envConfig.hasOwnProperty(_database)) throw Error(
+  if (!config.hasOwnProperty(_database)) throw Error(
     `Database config "${_database}" does not exist.` +
     'Verify that "config/data_models_storage_config.json" is correctly set.'
   )
-
 
   // Get and return the connection if it exists
   if (currentInstances.has(_database)) {
@@ -69,6 +53,6 @@ module.exports.getConnection = (database) => {
   }
 
   // Or create and return a new connection
-  currentInstances.set(_database, new Sequelize(envConfig[_database]))
+  currentInstances.set(_database, new Sequelize(config[_database]))
   return currentInstances.get(_database);
 }
