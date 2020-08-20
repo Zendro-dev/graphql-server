ef6fd6371b91944c46bb13165f86318c7be264d5
David Velasco <davvelsan@gmail.com>
2020-08-21 01:16:41 +0200

    feat: migrate to each database connection

--- migrateDbAndStartServer.sh
+++ migrateDbAndStartServer.sh
@@ -1,30 +1,49 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Wait until the relational database-server up and running
 waited=0
-until node ./utils/testSequelizeDbServerAvailable.js
+until node ./utils/testDatabaseConnectionsAvailable.js 1>/dev/null
 do
-	if [ $waited == 240 ]; then
-		echo -e '\nERROR: Time out reached while waiting for relational database server to be available.\n'
-		exit 1
-	fi
-	sleep 2
-	waited=$(expr $waited + 2)
+  if [ $waited == 240 ]; then
+    echo -e '\nERROR: Time out reached while waiting for relational database server to be available.\n'
+    exit 1
+  fi
+  sleep 2
+  waited=$(expr $waited + 2)
 done
 
-# Run the migrations 
-if ! ./node_modules/.bin/sequelize db:migrate; then
-	echo -e '\nERROR: Migrating the relational database(s) caused an error.\n'
-	exit 1
-fi
+
+# Run the migrations
+CONFIG="./config/data_models_storage_config.json"
+SEQUELIZE="./node_modules/.bin/sequelize"
+
+jq -r 'keys[] as $k | "\($k):\(.[$k] | .storageType)"' < $CONFIG |
+while read object; do
+
+  params=(${object//:/ })
+  key=${params[0]}
+  store=${params[1]}
+
+  if [[ "$store" == "sql" ]]; then
+
+    if ! $SEQUELIZE db:migrate --config "$CONFIG" --env $key; then
+      echo -e '\nERROR: Migrating the relational database(s) caused an error.\n'
+      exit 1
+    fi
+
+  fi
+
+done
+
 
 # Run seeders if needed
 if [ -d ./seeders ]; then
-	if ! ./node_modules/.bin/sequelize db:seed:all; then
-		echo -e '\nERROR: Seeding the relational database(s) caused an error.\n'
-		exit 1
-	fi
+  if ! $SEQUELIZE db:seed:all --config "$CONFIG" --env "sql"; then
+    echo -e '\nERROR: Seeding the relational database(s) caused an error.\n'
+    exit 1
+  fi
 fi
+
 
 # Start GraphQL-server
 npm start # acl
