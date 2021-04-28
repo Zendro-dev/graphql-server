52769b7466d1dd557b25c08941189d66274d72e5
coeit <cbeiteneuer@gmail.com>
2021-04-28 13:42:13 +0200

    refactor: metaquery accepts jq/jsonPath in the header + remove functionality for array of queries

--- server.js
+++ server.js
@@ -150,7 +150,7 @@ app.use(
 );
 
 let metaQueryCorsOptions = {
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", "jq", "jsonPath"],
 };
 app.options("/meta_query", cors(metaQueryCorsOptions));
 app.post("/meta_query", cors(), async (req, res, next) => {
@@ -164,40 +164,38 @@ app.post("/meta_query", cors(), async (req, res, next) => {
 
     if (req != null) {
       if ((await checkAuthorization(context, "meta_query", "")) === true) {
-        let queries = req.body.queries;
-        let jq = req.body.jq;
-        let jsonPath = req.body.jsonPath;
+        const query = req.body.query;
+        const jq = req.headers.jq;
+        const jsonPath = req.headers.jsonPath;
+        const variables = req.body.variables;
         helper.eitherJqOrJsonpath(jq, jsonPath);
 
-        if (!Array.isArray(queries)) {
-          let newQueries = [queries];
-          queries = newQueries;
-        }
-
-        let graphQlResponses = await helper.handleGraphQlQueriesForMetaQuery(
+        const graphQlResponse = await graphql(
           Schema,
+          query,
           resolvers,
-          queries,
-          context
+          context,
+          variables
         );
-        let output = null;
-
-        if (helper.isNotUndefinedAndNotNull(jq)) {
-          // jq
-          output = await nodejq
-            .run(jq, graphQlResponses, { input: "json", output: "json" })
-            .catch((err) => {
-              throw err;
+        let output = graphQlResponse.data;
+        if (output) {
+          if (helper.isNotUndefinedAndNotNull(jq)) {
+            // jq
+            output = await nodejq
+              .run(jq, graphQlResponse.data, { input: "json", output: "json" })
+              .catch((err) => {
+                throw err;
+              });
+          } else {
+            // JSONPath
+            output = JSONPath({
+              path: jsonPath,
+              json: graphQlResponse.data,
+              wrap: false,
             });
-        } else {
-          // JSONPath
-          output = JSONPath({
-            path: jsonPath,
-            json: graphQlResponses,
-            wrap: false,
-          });
+          }
         }
-        res.json(output);
+        res.json({ data: output, errors: graphQlResponse.errors });
         next();
       } else {
         throw new Error("You don't have authorization to perform this action");
