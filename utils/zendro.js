a59423c5bc4d02cc2f8a1215e132cbde98e2e3dd
wunderbarr <wunderbardan@gmail.com>
2022-08-16 11:06:14 +0200

    feat: enable token in migration

--- utils/zendro.js
+++ utils/zendro.js
@@ -10,6 +10,11 @@ const globals = require("../config/globals");
 let path = require("path");
 const { BenignErrorArray } = require("./errors");
 
+/**
+ * initializeZendro - initialize zendro object which provides the access to different APIs
+ * in zendro layers (resolvers, models, adapters) and enables graphql queries.
+ * @returns {object} zendro object
+ */
 module.exports.initializeZendro = async () => {
   await initializeStorageHandlers(models);
   await initializeStorageHandlers(adapters, "adapter");
@@ -21,7 +26,38 @@ module.exports.initializeZendro = async () => {
   };
 };
 
-const execute_graphql = async (query, variables, token) => {
+/**
+ * getToken - fetch token from Keycloak
+ * @returns {string} token
+ */
+const getToken = async () => {
+  const OAUTH2_TOKEN_URI = process.env.OAUTH2_TOKEN_URI;
+  const MIGRATION_USERNAME = process.env.MIGRATION_USERNAME;
+  const MIGRATION_PASSWORD = process.env.MIGRATION_PASSWORD;
+  const OAUTH2_CLIENT_ID = process.env.OAUTH2_CLIENT_ID;
+  const axios = require("axios");
+  const res = await axios({
+    method: "post",
+    url: `${OAUTH2_TOKEN_URI}`,
+    data: `username=${MIGRATION_USERNAME}&password=${MIGRATION_PASSWORD}&grant_type=password&client_id=${OAUTH2_CLIENT_ID}`,
+    headers: {
+      "content-type": "application/x-www-form-urlencoded;charset=utf-8",
+    },
+  });
+  if (res && res.data) {
+    return res.data.access_token;
+  } else {
+    throw new Error("Failed requesting an API token");
+  }
+};
+
+/**
+ * execute_graphql - execute graphql query or mutation
+ * @param {string} query query string
+ * @param {object} variables dynamic values for the query
+ * @returns {object} graphql response
+ */
+const execute_graphql = async (query, variables) => {
   try {
     let benign_errors_arr = new BenignErrorArray();
     let errors_sink = [];
@@ -29,7 +65,15 @@ const execute_graphql = async (query, variables, token) => {
       errors_sink.push(err);
     };
     benign_errors_arr.on("push", errors_collector);
-    let headers = token ? { headers: { authorization: token } } : null;
+    let token = null;
+    // fetch token when MIGRATION_USERNAME and MIGRATION_PASSWORD are defined
+    if (process.env.MIGRATION_USERNAME && process.env.MIGRATION_PASSWORD) {
+      token = await getToken();
+    }
+    // set token for queries related to a distributed setup
+    let headers = token
+      ? { headers: { authorization: "Bearer " + token } }
+      : null;
     let context = {
       request: headers,
       acl: null,
@@ -46,12 +90,28 @@ const execute_graphql = async (query, variables, token) => {
     );
     if (errors_sink.length > 0) {
       for (let err of errors_sink) {
+        if (
+          err &&
+          err.errno == -3001 &&
+          err.code == "EAI_AGAIN" &&
+          err.syscall == "getaddrinfo"
+        ) {
+          err = new Error(
+            `You are using Docker and a distributed setup. Did you run this migration from the host?\n` +
+              `If so, some host-names might not be reachable, but they are from within the GraphQL-server's Docker container.\n` +
+              `Suggest to run the migration from within the GraphQL-server's containter directly. `
+          );
+        }
         graphQlResponse.errors = graphQlResponse.errors
           ? graphQlResponse.errors.concat(err)
           : [err];
       }
     }
-    return { data: graphQlResponse.data, errors: graphQlResponse.errors };
+    let result = { data: graphQlResponse.data };
+    if (graphQlResponse.errors) {
+      result["errors"] = graphQlResponse.errors;
+    }
+    return result;
   } catch (error) {
     return { data: null, errors: [formatError(error)] };
   }
